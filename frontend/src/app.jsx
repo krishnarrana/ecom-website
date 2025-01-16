ae1ba84fc0eaa06b85735c699de9995c3a84c392
krishnarrana <krishna2121.rana@gmail.com>
2025-01-16 21:07:10 +0100

    Footer and Header Added

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,24 @@
 import { createRoot } from "react-dom/client";
 
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+
+import { Container } from "react-bootstrap";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Foorter";
+
 const App = () => {
   return (
-    <div>
-      <h1>Front End</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
